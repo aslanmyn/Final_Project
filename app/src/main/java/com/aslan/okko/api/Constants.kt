c0aa8385d623f2a8574a8ac6aa6310cf8b5b1cbf
aslanmyn <mustafaev.aslan@55gmail.com>
2024-05-17 16:02:23 +0500

    api upgrade

--- app/src/main/java/com/aslan/okko/api/Constants.kt
+++ app/src/main/java/com/aslan/okko/api/Constants.kt
@@ -1,6 +1,8 @@
 package com.aslan.okko.api
 
+import com.aslan.okko.BuildConfig
+
 object Constants {
-    const val apiKey = "44f514f3a7416ef8cd68b3bcf7211caa"
-    const val accessToken= "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NGY1MTRmM2E3NDE2ZWY4Y2Q2OGIzYmNmNzIxMWNhYSIsInN1YiI6IjY2MzRjYTZmZTkyZDgzMDEyYWQyYTQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h2df4mjcA2jegczUJVpyWTlR2C8t2Z5P2mrtuZyFpOw"
+    const val apiKey = BuildConfig.API_KEY
+    const val accessToken = BuildConfig.ACCESS_TOKEN
 }
